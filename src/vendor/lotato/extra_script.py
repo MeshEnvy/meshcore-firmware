7fa048e97296735998a1335dd40b50cb131e9782
Ben Allfree <ben@benallfree.com>
2026-04-21 06:15:17 -0700

    chore: migrate lotato

--- src/vendor/lotato/extra_script.py
+++
@@ -1,4 +0,0 @@
-Import("env")
-
-# Lotato includes firmware headers (e.g. MeshCore.h, helpers/AdvertDataHelpers.h) from this repo.
-env.Append(CPPPATH=["$PROJECT_DIR/src"])
--- src/vendor/lotato/lotato_ingest_ttl.pb.c
+++
@@ -1,8 +0,0 @@
-/* nanopb constant definitions for Lotato ingest TTL */
-#include "lotato_ingest_ttl.pb.h"
-
-#if PB_PROTO_HEADER_VERSION != 40
-#error Regenerate this file with the current version of nanopb generator.
-#endif
-
-PB_BIND(LotatoIngestTtl, LotatoIngestTtl, AUTO)
